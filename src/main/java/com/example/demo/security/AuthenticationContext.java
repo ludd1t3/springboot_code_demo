577f145906f92158b17dcbfaa2da56d91d6f5c25
Jason Herlevi <jason.herlevi@domo.com>
2023-11-02 08:42:34 -0600

    + AuthenticationContext

---
+++ src/main/java/com/example/demo/security/AuthenticationContext.java
@@ -0,0 +1,15 @@
+package com.example.demo.security;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public class AuthenticationContext {
+    public String userId() {
+        return "fa7d5b9e-8e5a-4b0e-9b0a-8b1f1b5c8e2e";
+    }
+
+    public List<String> grants() {
+        return List.of("read:invoices", "write:invoices");
+    }
+}
